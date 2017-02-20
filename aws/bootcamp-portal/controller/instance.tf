826e29131d67fe84ac9e71a9f1e87e682cd7ecb4
Ed Hull <ed.hull@capgemini.com>
2017-02-20 16:27:03 +0000

    Updated ami to use public IP

--- aws/bootcamp-portal/controller/instance.tf
+++ aws/bootcamp-portal/controller/instance.tf
@@ -1,8 +1,23 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
 resource "aws_instance" "controller" {
     ami = "${data.aws_ami.ubuntu.id}"
-    instance_type = "t2.micro"
+    instance_type = "${var.instancetype}"
     vpc_security_group_ids = [""]
+    associate_public_ip_address = true
+    key_name = "${var.ssh_key_name}"
     tags {
-        Name = "HelloWorld"
+         Name = "${var.environment}-controller"
     }
 }
+
