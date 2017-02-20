ea5544ad76a89fc5a500071b4f62e907ce1a610b
Ed Hull <ed.hull@capgemini.com>
2017-02-20 14:15:14 +0000

    Set staging/production variable & vpc name

--- aws/bootcamp-portal/variables.tf
+++ aws/bootcamp-portal/variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   description = "The region of AWS where the instance should be created."
   default = "eu-west-1"
 }
+
+variable "environment" {
+  description = "Staging or Production"
+}
+
