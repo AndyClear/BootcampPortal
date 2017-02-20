8544d66f025aa128067b40134772295da80ba8f6
Teodor Kostov <teodor.kostov@container-solutions.com>
2017-02-20 18:05:24 +0100

    Removing some extra information.

--- aws/bootcamp-portal/subnets.tf
+++ aws/bootcamp-portal/subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "main" {
     count = "3"
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.${count.index}.0/24"
-    availability_zone = "${element(data.aws_availability_zones.available.names, count.index%3)}"
+    availability_zone = "${element(data.aws_availability_zones.available.names, count.index % 3)}"
     map_public_ip_on_launch = false
     tags {
         Name = "subnet-${var.environment}"
--- aws/bootcamp-portal/variables.tf
+++ aws/bootcamp-portal/variables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-  description = "The region of AWS where the instance should be created."
-  default = "eu-west-1"
-}
-
 variable "environment" {
   description = "Staging or Production"
 }
