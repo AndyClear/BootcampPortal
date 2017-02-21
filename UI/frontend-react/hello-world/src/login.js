e7ceb60bbd1b3353c7c6d4fd4c137d531f877ea6
alhinde <alastair.hinde@capgemini.com>
2017-02-21 18:06:48 +0000

    login inputs

--- UI/frontend-react/hello-world/src/login.js
+++ UI/frontend-react/hello-world/src/login.js
@@ -1,11 +1,27 @@
 /**
  * Created by AHINDE on 21/02/2017.
  */
-import React from 'react'
+import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+
+const style = {
+    margin: 12,
+}
 
 class Login extends React.Component {
     render() {
-        return <h1>Login</h1>
+        return (
+        <div><h1>Login</h1>
+            <TextField hintText="Username" />
+            <br/>
+            <TextField hintText="Password" type="password" />
+            <br />
+            <RaisedButton label="Login" primary={true} style={style} />
+        </div>
+    )
+
     }
 }
 
