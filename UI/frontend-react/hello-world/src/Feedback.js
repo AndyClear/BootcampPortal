bd97f76c842b8284793ae7eb94fe66541c711e51
Clear <andy.clear@capgemini.com>
2017-02-22 14:53:26 +0000

    Edited locations and added Feedback

---
+++ UI/frontend-react/hello-world/src/Feedback.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+  Step,
+  Stepper,
+  StepLabel,
+} from 'material-ui/Stepper';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+
+class Feedback extends React.Component {
+
+  state = {
+    finished: false,
+    stepIndex: 0,
+  };
+
+  handleNext = () => {
+    const {stepIndex} = this.state;
+    this.setState({
+      stepIndex: stepIndex + 1,
+      finished: stepIndex >= 2,
+    });
+  };
+
+  handlePrev = () => {
+    const {stepIndex} = this.state;
+    if (stepIndex > 0) {
+      this.setState({stepIndex: stepIndex - 1});
+    }
+  };
+
+  getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return 'Question 1';
+      case 1:
+        return 'Question 2';
+      case 2:
+        return 'Question 3';
+      default:
+        return 'Feedback';
+    }
+  }
+
+  render() {
+    const {finished, stepIndex} = this.state;
+    const contentStyle = {margin: '0 16px'};
+    const steps = ["How did you find the instructors?", "How did you find the facilities?", "Anything else you would add?"]
+
+
+
+    return (
+      <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
+        <Stepper activeStep={stepIndex}>
+          <Step>
+            <StepLabel>How did you find the instructors?</StepLabel>
+            <TextField hintText="Answer here" />
+          </Step>
+          <Step>
+            <StepLabel>How did you find the facilities?</StepLabel>
+            <TextField hintText="Answer here" />
+          </Step>
+          <Step>
+            <StepLabel>Anything else you would add?</StepLabel>
+            <TextField hintText="Answer here" />
+          </Step>
+        </Stepper>
+        <div style={contentStyle}>
+          {finished ? (
+            <p>
+              <a
+                href="#"
+                onClick={(event) => {
+                  event.preventDefault();
+                  this.setState({stepIndex: 0, finished: false});
+                }}
+              >
+                Click here
+              </a> to reset the example.
+            </p>
+          ) : (
+            <div>
+              <p>{this.getStepContent(stepIndex)}</p>
+              <div style={{marginTop: 12}}>
+                <FlatButton
+                  label="Back"
+                  disabled={stepIndex === 0}
+                  onTouchTap={this.handlePrev}
+                  style={{marginRight: 12}}
+                />
+                <RaisedButton
+                  label={stepIndex === 2 ? 'Finish' : 'Next'}
+                  primary={true}
+                  onTouchTap={this.handleNext}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Feedback
--- UI/frontend-react/hello-world/src/locations.js
+++ UI/frontend-react/hello-world/src/locations.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import Map from 'google-maps-react'
 
   class Locations extends React.Component {
   render() {
   return (
+  	<div>
+
+  		<div>
+  			<h2>Locations</h2>
+  		</div>
+
+  		<div>
+  			<h3>Capgemini Holborn Office</h3>
+  			<div>
+  			<Map google={this.props.google}
+  			style={{width: '500px', height: '500px', position: 'absolute'}} 
+  			zoom={14}>
+  			</Map> 
+  			</div> 			
+  		</div>
+
+  	</div>
+  	)
+  }
+}
+
+
+export default Locations;
