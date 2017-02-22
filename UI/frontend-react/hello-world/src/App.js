e3f90621d3cdf9a8b6fb09ce5ffcce97584ea33d
magdalenek <ms915@ic.ac.uk>
2017-02-22 14:31:25 +0000

    adding better functionality to menu

--- UI/frontend-react/hello-world/src/App.js
+++ UI/frontend-react/hello-world/src/App.js
@@ -4,15 +4,25 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
 import Toggle from 'material-ui/Toggle';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import Drawer from 'material-ui/Drawer';
 import RaisedButton from 'material-ui/RaisedButton';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+const nearbyIcon = <IconLocationOn />;
+const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
+const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
 import {Link} from 'react-router';
 import './App.css';
 
 class App extends Component {
+
+state = {
+    selectedIndex: 0,
+  };
 
 style = {
   height: 100,
@@ -27,12 +37,14 @@ style = {
     this.state = {open: false};
   }
 
+  select = (index) => this.setState({selectedIndex: index});
+
   handleToggle = () => this.setState({open: !this.state.open});
 
   render() {
     return (
       <div>
-       <AppBar title="BootCamp Portal" iconClassNameRight="muidocs-icon-navigation-expand-more" onClick={this.handleToggle}></AppBar>
+       <AppBar title="BootCamp Portal" iconClassNameRight="muidocs-icon-navigation-expand-more" onClick={this.handleToggle}> </AppBar>
         <div className="App">
           <div className="App-header">
           <div>
@@ -41,16 +53,16 @@ style = {
             width={200}
             open={this.state.open} 
             onRequestChange={(open) => this.setState({open})} >
-            <AppBar title="Menu" showMenuIconButton={false} onTouchTap={this.handleToggle} />
-            <MenuItem><Link to="/dashboard">Dashboard</Link></MenuItem>
-            <MenuItem><Link to="/login">Login</Link></MenuItem>
+            <AppBar title="Menu" showMenuIconButton={false} onClick={this.handleToggle} />
+            <MenuItem containerElement={<Link to="/dashboard"/>}>Dashboard</MenuItem>
+            <MenuItem containerElement={<Link to="/login"/>}>Login</MenuItem>
           </Drawer>
           {this.props.children}
           </div>
       </div>
       </div>
-      
       </div>
+   
     );
   }
 }
