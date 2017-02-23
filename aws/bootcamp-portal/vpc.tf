507e0b15a949e7b8378664a0c0326bf45e1453cb
Chris Wilkinson <chris@chriswilkinson.info>
2017-02-23 11:27:13 +0000

    Added vpc peering connection

--- aws/bootcamp-portal/vpc.tf
+++ aws/bootcamp-portal/vpc.tf
@@ -6,5 +6,16 @@ resource "aws_vpc" "main" {
 
 }
 
+data "aws_vpc" "drone_vpc" {
+    cidr_block = "10.99.0.0/16"
+}
 
-
+resource "aws_vpc_peering_connection" "vpc_peering_connection" {
+    vpc_id = "${aws_vpc.main.id}"
+    peer_vpc_id = "${data.aws_vpc.drone_vpc.id}"
+    auto_accept = true
+    peer_owner_id = "${var.peer_owner_id}"
+    tags {
+        Name = "CI-Prod Peering Connection"
+    }
+}
