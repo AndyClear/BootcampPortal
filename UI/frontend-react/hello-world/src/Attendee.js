60252d2b3bb95cb55c987e74c258abc1f270da0c
magdalenek <ms915@ic.ac.uk>
2017-02-23 14:28:39 +0000

    fixing component

--- UI/frontend-react/hello-world/src/Attendee.js
+++ UI/frontend-react/hello-world/src/Attendee.js
@@ -1,8 +1,5 @@
-/**
- * Created by AHINDE on 21/02/2017.
- */
+
 import React from 'react';
-//import MobileTearSheet from '../../../MobileTearSheet';
 import {List,ListItem} from 'material-ui/List';
 import ActionGrade from 'material-ui/svg-icons/action/grade';
 import Divider from 'material-ui/Divider';
@@ -10,32 +7,30 @@ import Avatar from 'material-ui/Avatar';
 import {  pinkA200, transparent} from 'material-ui/styles/colors';
 import './Attendee.css';
 
+const names = ["Chelsea Otakan","Eric Hoffman", "James Anderson"];
+const avatars = ["http://www.material-ui.com/images/chexee-128.jpg", "http://www.material-ui.com/images/kolage-128.jpg", "http://www.material-ui.com/images/jsa-128.jpg"];
+
 class Attendee extends React.Component {
+
+row = () => {
+   var rows = [];
+   for (let i = 0; i<names.length; i++) {
+       rows.push(<ListItem primaryText={names[i]} rightAvatar={<Avatar src={avatars[i]} />}/>);
+   }
+   return rows;
+}
         render() {
             return (
-
               <div>
-                <h1 > Attendee < /h1>
-
-          
-
-                <List >
-                    <ListItem primaryText="Chelsea Otakan" leftIcon={< ActionGrade color={pinkA200}/>}rightAvatar={< Avatar src="http://www.material-ui.com/images/chexee-128.jpg" />}/>
-                    <ListItem primaryText="Eric Hoffman" insetChildren={true} rightAvatar={< Avatar src="http://www.material-ui.com/images/kolage-128.jpg" />}/>
-                    <ListItem primaryText="James Anderson" insetChildren={true} rightAvatar={ < Avatar src="http://www.material-ui.com/images/jsa-128.jpg" />}/>
-                    <ListItem primaryText="Kerem Suer" insetChildren={true} rightAvatar={< Avatar src="http://www.material-ui.com/images/kerem-128.jpg" />}/>
-               </List>
-                    <Divider inset={true}/>
-               <List >
-                    <ListItem primaryText="Adelle Charles" leftAvatar={<Avatar color={pinkA200} backgroundColor={transparent} style={{left: 8}}>A</Avatar>}rightAvatar={< Avatar src="http://www.material-ui.com/images/adellecharles-128.jpg" />}/>
-                    <ListItem primaryText="Adham Dannaway" insetChildren={true}rightAvatar={<Avatar src="http://www.material-ui.com/images/adhamdannaway-128.jpg" />}/>
-                    <ListItem primaryText="Allison Grayce" insetChildren={true}rightAvatar={< Avatar src="http://www.material-ui.com/images/allisongrayce-128.jpg" />}/>
-                    <ListItem primaryText="Angel Ceballos" insetChildren={true}rightAvatar={<Avatar src="http://www.material-ui.com/images/angelceballos-128.jpg" />}/>
-               </List>
+                <h1 >Atendees< /h1>
+                  <tbody>
+                    {this.row()}  
+                  </tbody>
                </div>
-
                 )
             }
         }
 
+
+      
 export default Attendee;
