98192994f450d0d34b957774c7f3000802ae79ed
magdalenek <ms915@ic.ac.uk>
2017-02-23 15:33:27 +0000

    adding modal to display extra information

--- UI/frontend-react/hello-world/src/dashboard.js
+++ UI/frontend-react/hello-world/src/dashboard.js
@@ -1,16 +1,41 @@
 import React from 'react'
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import {List,ListItem} from 'material-ui/List';
 
 class Dashboard extends React.Component {
 
+
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+
  render() {
-     const ret = [];
+    const ret = [];
     const timeList = ["09:30", "12:15", "13:15", "17:00"];
     const itemList = ["Introduction to Microservices. Location: Code Node", "Lunch", "Practical session: using docker", "Day 1 finish"]
-    for (let i = 0; i < timeList.length; i++) {
-        ret[i] = <li>{" " + timeList[i] + " - " + itemList[i]}</li>
 
-        }
+      const actions = [
+      <FlatButton
+        label="OK"
+        primary={true}
+        onClick={this.handleClose}
+      />
+    ];   
+    for (let i = 0; i < timeList.length; i++) {
+        ret[i] = <ListItem><a onClick={this.handleOpen} >{" " + timeList[i] + " - " + itemList[i]}</a></ListItem> //add api item 
+    }
+
 
   return (
   <div>
@@ -23,6 +48,17 @@ class Dashboard extends React.Component {
         <ul>
         {ret}
         </ul>
+      </div>
+      <div>
+        <Dialog
+          title="Title"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          The actions in this window were passed in as an array of React objects.
+        </Dialog>
       </div>
   </div>
   )
