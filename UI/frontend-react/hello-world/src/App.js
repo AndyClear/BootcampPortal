017f6e5594d2d3f37b6b77feaf0f533bfbc00632
magdalenek <ms915@ic.ac.uk>
2017-02-23 11:37:08 +0000

    adding copyright

--- UI/frontend-react/hello-world/src/App.js
+++ UI/frontend-react/hello-world/src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
-//import IconButton from 'material-ui/IconButton';
-//import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
-//import FlatButton from 'material-ui/FlatButton';
-//import FontIcon from 'material-ui/FontIcon';
-//import Toggle from 'material-ui/Toggle';
-//import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import Drawer from 'material-ui/Drawer';
-//import RaisedButton from 'material-ui/RaisedButton';
-//import Paper from 'material-ui/Paper';
-//import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+import FontIcon from 'material-ui/FontIcon';
+import Toggle from 'material-ui/Toggle';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import RaisedButton from 'material-ui/RaisedButton';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 import ImageEdit from 'material-ui/svg-icons/image/edit';
 import ActionFace from 'material-ui/svg-icons/action/face'
-//import ActionFavorite from 'material-ui/svg-icons/action/favorite'
 import MapsPlace from 'material-ui/svg-icons/maps/place';
 import ActionHome from 'material-ui/svg-icons/action/home';
+import ActionCopyright from 'material-ui/svg-icons/action/copyright';
 import ContentWeekend from 'material-ui/svg-icons/content/weekend';
 import {Link} from 'react-router';
 import './App.css';
@@ -27,7 +23,7 @@ class App extends Component {
     this.state = {open: false};
   }
 
-  handleToggle = () => this.setState({open: !this.state.open});
+   handleToggle = () => this.setState({open: !this.state.open});
 
   render() {
     return (
@@ -39,17 +35,15 @@ class App extends Component {
           <Drawer
             docked={false}
             width={200}
-            open={this.state.open}
-            onRequestChange={(open) => this.setState({open})} >
+            open={this.state.open} >
             <AppBar title="Menu" showMenuIconButton={false} onClick={this.handleToggle} />
             <MenuItem containerElement={<Link to="/dashboard"/>} leftIcon={<ActionHome />}>Dashboard</MenuItem>
             <MenuItem containerElement={<Link to="/login"/>} leftIcon={<ActionFace />} > Login</MenuItem>
             <MenuItem containerElement={<Link to="/feedback"/>} leftIcon={<ImageEdit />} > Feedback</MenuItem>
             <MenuItem containerElement={<Link to="/locations"/>} leftIcon={<MapsPlace /> }>Locations</MenuItem>
             <MenuItem containerElement={<Link to="/Attendee"/>} leftIcon={<ContentWeekend /> }> Attendees</MenuItem>
-            <MenuItem className="Bottom">Capgemini</MenuItem>
+            <h5 className="Bottom"> &copy; Devops Bootcamp 2017</h5>
           </Drawer>
-
           {this.props.children}
           </div>
       </div>
